03cddf57d94d83e17121ba72930780860c0f9a3a
bowenmiao-tech <bowen.miao.b.m@gmail.com>
2026-04-13 23:03:33 +1000

    change sql

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+window.TECHM8_SUPABASE = {
+  url: 'https://abkjbhmifswfexpjkval.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFia2piaG1pZnN3ZmV4cGprdmFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwNTYwNzYsImV4cCI6MjA5MTYzMjA3Nn0.3xA0CEIJgIC3EXXhhAZDNxlMyRlEKGsv2xO1UkpNEWc'
+};
